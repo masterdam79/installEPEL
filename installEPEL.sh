778e2be5282f098b5789384d5d51566d86b18be9
Richard Reijmers <rreijmers@denit.nl>
2015-06-29 12:32:24 +0200

    wijzigingen

--- installEPEL.sh
+++ installEPEL.sh
@@ -6,9 +6,9 @@
 # Some variables
 EPELREPOFILE="/etc/yum.repos.d/epel.repo"
 RHELVERSIONFILE="/etc/redhat-release"
-RHELMAINVERSION=`egrep -o "[0-9]" /etc/redhat-release | head -1`
+RHELMAINVERSION=`egrep -o "[0-9]" ${RHELVERSIONFILE} | head -1`
 ARCH=`uname -m`
-EPELBASEURL="http://dl.fedoraproject.org/pub/epel/${RHELMAINVERSION}/${ARCH}/e/"
+EPELBASEURL="http://dl.fedoraproject.org/pub/epel/${RHELMAINVERSION}/${ARCH}/"
 EPELLATESTVERSIONFILE=`links -dump ${EPELBASEURL} | grep epel-release | awk '{ print $2 }' | grep "http" | awk -F'/' '{print $9}' | sort | tail -1`
 
 # Some pretty colors
